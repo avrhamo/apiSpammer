965ea61ae6852edfa73f2f03ddb675917bace5db
avraham <avrhamo@gmail.com>
2024-04-16 21:26:41 +0300

    init

--- src/screens/field-matching.js
+++ src/screens/field-matching.js
@@ -1,20 +1,5 @@
 
-const curlInput = '{\n' +
-    '  "method": "OPTIONS",\n' +
-    '  "headers": {\n' +
-    '    "accept": "*/*",\n' +
-    '    "accept-language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",\n' +
-    '    "access-control-request-headers": "authorization",\n' +
-    '    "access-control-request-method": "POST",\n' +
-    '    "origin": "https://mail.google.com",\n' +
-    '    "referer": "https://mail.google.com/",\n' +
-    '    "sec-fetch-dest": "empty",\n' +
-    '    "sec-fetch-mode": "cors",\n' +
-    '    "sec-fetch-site": "same-site",\n' +
-    '    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36"\n' +
-    '  },\n' +
-    '  "url": "https://play.google.com/log?format=json&hasfast=true"\n' +
-    '}\n';
+const curlInput = '{"method":"GET","headers":{"authority":"login.bankhapoalim.co.il","accept":"application/json, text/plain, */*","accept-language":"he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7","content-type":"application/json;charset=UTF-8","$cookie":"_cls_v=b9460df9-b310-40bf-b065-8ab55c870260; bnhpremarketing=","referer":"https://login.bankhapoalim.co.il/ng-portals/rb/he/plastic-cards/order","sec-ch-ua":"\\"Chromium\\";v=\\"112\\", \\"Google Chrome\\";v=\\"112\\", \\"Not:A-Brand\\";v=\\"99\\"","sec-ch-ua-mobile":"?0","sec-ch-ua-platform":"\\"macOS\\"","sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-origin","user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36","x-api-version":"2","x-b3-traceid":"60e464a7-3393-da14-5c3c-32d6a240adc8","x-xsrf-token":"44d150ef9fd5736621a8ada34d09405e72076a1de3bf8cf852d755224ddeb773","Accept-Encoding":"deflate, gzip"},"url":"https://login.bankhapoalim.co.il/bnhp-api/cards-channels/web-proposal/v1/proposals/credit-lobby?accountId=12-544-468629&_lang=he-IL"}';
 
 const parsedCurlInput = JSON.parse(curlInput);
 
@@ -54,9 +39,9 @@ function generateTableRows(components) {
         // Add rows for query parameters
         urlObject.searchParams.forEach((value, key) => {
             tableRows += `<tr>
-                <td>queryParam.${key}</td>
+                <td>${key}</td>
                 <td>
-                    <select class="dropdown" data-key="queryParam.${key}">
+                    <select class="dropdown" data-key="${key}">
                         <!-- Dropdown options will be populated dynamically -->
                     </select>
                 </td>
@@ -101,33 +86,36 @@ function generateTableRows(components) {
     return tableRows;
 }
 
-function populateDropdowns(components) {
+function populateDropdowns(components, prefix = '') {
     for (let key in components) {
         if (components.hasOwnProperty(key)) {
             const value = components[key];
             const dropdown = document.querySelector(`[data-key="${key}"]`);
 
-            if (dropdown) {
-                // Clear previous options
-                dropdown.innerHTML = '';
+            if (dropdown || typeof value === 'object') {
+                // Clear previous options if dropdown exists
+                if (dropdown) {
+                    dropdown.innerHTML = '';
+                }
 
                 // Populate options
                 // Check if the value is an object (for nested properties)
                 if (typeof value === 'object' && value !== null) {
-                    for (let subKey in value) {
-                        const optionElement = document.createElement('option');
-                        optionElement.text = value[subKey];
-                        dropdown.add(optionElement);
-                    }
+                    // Recursively populate dropdowns for nested properties
+                    populateDropdowns(value, `${prefix}${key}.`);
                 } else {
                     // If value is not an object, add a single option with the value
                     const optionElement = document.createElement('option');
                     optionElement.text = value;
-                    dropdown.add(optionElement);
+                    if (dropdown) {
+                        dropdown.add(optionElement);
+                    }
                 }
 
                 // Set default value if it exists in the options
-                dropdown.value = value;
+                if (dropdown) {
+                    dropdown.value = value;
+                }
             }
         }
     }
